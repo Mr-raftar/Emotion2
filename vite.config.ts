324dd6ae1d49c5bb52ae8d26f3081b9e517ba1f1
kartikchavan <karu16696@gmail.com>
2025-03-07 23:20:52 +0530

    Deploying site

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Emotion2/", // Ensure correct asset loading on GitHub Pages
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
